eeb0bb6aba473fb3e4115ce323fdc31349697d69
GT013 <42264121+GT013@users.noreply.github.com>
2020-09-30 15:47:03 +0700

    m

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
-app.get('/',(req, res) => {
+/**app.get('/',(req, res) => {
     res.send({ping:'hello this is server and I am alive!'});
-})
-app.listen(3011);
+})**/
 
+mongoose.connect('mongodb://localhost/mydatabase');
+var db = mongoose.connection;
+
+db.on('error', function callback () {
+    console.log("Connection error");
+  });
+  
+  db.once('open', function callback () {
+    console.log("Mongo working!");
+  });
+  
+app.listen(3011);
+console.log('Listening on port 3011...');
