215da1961149709d588ebffe6b1f574d6922fa06
GT013 <42264121+GT013@users.noreply.github.com>
2020-09-30 15:33:05 +0700

    add

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/ping', function(req, res) {
+app.get('/s60030049', function(req, res) {
     res.send({ping:'hello this is server and I am alive!'});
 })
 
-app.get('/ping/:id', function(req, res) {
+app.get('/s60030049/:id', function(req, res) {
     res.send({ping:'hello this is server and I am got '+req.params.id});
 });
 
