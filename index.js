c47c533c684bbc1fd8e923bf07d36199537dbd30
GT013 <42264121+GT013@users.noreply.github.com>
2020-09-30 15:41:19 +0700

    a

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/ping',function(req, res) {
+app.get('/',(req, res) => {
     res.send({ping:'hello this is server and I am alive!'});
 })
 app.listen(3011);
-console.log('Listening on port 3011...');
+
