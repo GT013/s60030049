86663e6a98e85f77754b7934e175adcf98756de8
GT013 <42264121+GT013@users.noreply.github.com>
2020-10-20 22:42:37 +0700

    add

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,8 +1,10 @@
+var embedded = require('mongoose').model('embedded');
 exports.render = function(req,res){
     res.render('index',{
         'title':'Hello Jade'
     });
 };
 exports.embedded = function(req,res){
-    res.json(req)
+    var emb = embedded.insertMany(req.body)
+    res.json(emb)
 }
